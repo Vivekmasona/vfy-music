89e4a20f895218e93a4af61a513dd34301deea60
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-02-20 19:17:35 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,26 +17,25 @@ export default defineConfig({
         ],
       },
       manifest: {
-        name: 'Hyperpipe',
-        short_name: 'Hyperpipe',
+        name: 'VIVEKFY-MUSIC',
+        short_name: 'VFY-MUSIC',
         start_url: '/',
         display: 'standalone',
         background_color: '#000',
         theme_color: '#000',
-        description: 'Privacy respecting YouTube Music Frontend.',
-        icons: [
+        description: 'DEVELOPER vivekmasona: [
           {
-            src: '/android-chrome-192x192.png',
+            src: '/vivek.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/android-chrome-512x512.png',
+            src: '/vivek.png',
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/android-maskable.png',
+            src: '/vivek.png',
             sizes: '1024x1024',
             type: 'image/png',
             purpose: 'maskable',
