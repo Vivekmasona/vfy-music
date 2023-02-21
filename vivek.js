354d87bf69d42e6721782c09e3bb520550b83864
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-02-21 20:07:38 +0530

    Create vivek.js

---
+++ vivek.js
@@ -0,0 +1,32 @@
+[
+	{
+		"name": "hyperpipeapi.onrender.com (Official)",
+		"locations": "🇩🇪",
+		"api_url": "https://hyperpipeapi.onrender.com",
+		"org": "Render"
+	},
+	{
+		"name": "hyperpipeapi.esmailelbob.xyz",
+		"locations": "🇨🇦",
+		"api_url": "https://hyperpipeapi.esmailelbob.xyz",
+		"org": "OVH SAS"
+	},
+	{
+		"name": "hyperpipeapi.esmail5pdn24shtvieloeedh7ehz3nrwcdivnfhfcedl7gf4kwddhkqd.onion",
+		"locations": "🇨🇦",
+		"api_url": "http://hyperpipeapi.lqs5fjmajyp7rvp4qvyubwofzi6d4imua7vs237rkc4m5qogitqwrgyd.onion",
+		"org": "OVH SAS"
+	},
+	{
+		"name": "listenapi.whatever.social",
+		"locations": "🇺🇸",
+		"api_url": "https://listenapi.whatever.social",
+		"org": "Advin"
+	},
+	{
+		"name": "musicapi.adminforge.de",
+		"locations": "🇩🇪",
+		"api_url": "https://musicapi.adminforge.de",
+		"org": "adminforge.de"
+	}
+]
