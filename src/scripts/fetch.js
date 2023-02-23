46c77ef3e2f669293ebc8462a5dbf15f7d8fed9f
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-02-23 13:58:37 +0530

    Update fetch.js

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -1,6 +1,6 @@
 import { useStore, useSanitize } from './util.js';
 
-export const PIPED_INSTANCE = 'vfy.netlify.app';
+export const PIPED_INSTANCE = 'piped.pages.dev';
 export const HYPERPIPE_INSTANCE = 'hyperpipeapi.esmailelbob.xyz';
 
 export function getPipedQuery() {
