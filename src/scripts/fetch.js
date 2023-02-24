62f40319478f110bf596464cd5423495805a83d6
Vivekmasona <112853103+Vivekmasona@users.noreply.github.com>
2023-02-24 12:08:25 +0530

    Update fetch.js

--- src/scripts/fetch.js
+++ src/scripts/fetch.js
@@ -1,6 +1,6 @@
 import { useStore, useSanitize } from './util.js';
 
-export const PIPED_INSTANCE = 'piped.mha.fi';
+export const PIPED_INSTANCE = 'do.piped.video';
 export const HYPERPIPE_INSTANCE = 'hyperpipeapi.esmailelbob.xyz';
 
 export function getPipedQuery() {
